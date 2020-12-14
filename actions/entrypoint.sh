350191064fd9109117fc96994a98931263c44f76
Thomas Demarcy <thde@oticonmedical.com>
2020-12-14 11:37:19 +0100

    fix sed error

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /github/workspace
-sed -i "s/gitSHA/${GITHUB_SHA::7}" resume.tex
+sed -i "s/gitSHA/${GITHUB_SHA::7}/" resume.tex
 xelatex resume.tex
 sed -i "s/${GITHUB_SHA::7}/gitSHA/" resume.tex
 
